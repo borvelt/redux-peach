28eb4d217f218de2dcd3c6155e5daa3d33edfc57
borvelt <borvelt@gmail.com>
2018-09-13 17:25:42 +0430

    initial commit - package tested

--- src/Store.js
+++
@@ -1,9 +0,0 @@
-const redux = require('redux')
-const composedEnhacers = require('./Enhancers')
-
-const Store = redux.createStore(() => ({}), composedEnhacers)
-
-Store.Handlers = {}
-Store.Actions = {}
-
-module.exports = Store
